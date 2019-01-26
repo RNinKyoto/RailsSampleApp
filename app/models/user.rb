b4d2dbaa65885d9ff9831251832b341220912926
RNinKyoto <shinkan0330@outlook.jp>
2019-01-26 23:47:13 +0900

    adds Devise to username

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,9 +19,17 @@
 #  locked_at              :datetime
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  name                   :string(255)
 #
 
 class User < ApplicationRecord
+  validates :name,
+            presence: true,
+            uniqueness: { case_sensitive: false }
+
+  validates_format_of :name, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
+  validate :validate_name
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -29,5 +37,23 @@ class User < ApplicationRecord
          :confirmable, :lockable, :timeoutable
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
+  end
+
+  def login
+    @login || name || email
+  end
+
+  def validate_name
+    errors.add(:name, :invalid) if User.where(email: name).exists?
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    conditions[:email]&.downcase!
+    login = conditions.delete(:login)
+
+    where(conditions.to_hash).where(
+      ['lower(name) = :value OR lower(email) = :value', { value: login.downcase }]
+    ).first
   end
 end
