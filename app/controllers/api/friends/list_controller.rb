c399eb216d33a7bbc8eb60ab6a8e551de1297552
RNinKyoto <shinkan0330@outlook.jp>
2019-02-21 00:01:20 +0900

    deletes Chapter9

--- app/controllers/api/friends/list_controller.rb
+++
@@ -1,9 +0,0 @@
-module API::Friends
-  class ListController < ApplicationController
-    include Debuggable
-
-    def index
-      log_yo
-    end
-  end
-end
--- app/models/concerns/comment_validator.rb
+++
@@ -1,7 +0,0 @@
-module CommentValidator
-  extend AcctiveSupport::Concern
-
-  included do
-    validates :body, presence: true, length: { maximum: 140 }
-  end
-end
